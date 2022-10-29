0369f9d8d432e54d2dd200f254d43d404764876b
Hobiniaina Mirado RAMAHATSANGIARISON <miradotsangy@gmail.com>
2022-10-29 12:56:25 +0300

    create product component

--- src/app/features/catalog/pages/product/product.component.ts
+++ src/app/features/catalog/pages/product/product.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
+import { BreadCrumb } from 'src/app/shared/models/bread-crumb/bread-crumb.model';
+import { ExportService } from 'src/app/shared/serives/export/export.service';
+import { FileService } from 'src/app/shared/serives/file/file.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
+  public title: string = 'Articles';
+  public breadCrumbs: BreadCrumb[] = [];
+  private subscription = new Subscription();
+  public productNumber: number = 0;
 
-  constructor() { }
+  constructor(
+    private fileService: FileService,
+    private notificationService: NotificationService,
+    private exportService: ExportService
+  ) { }
 
   ngOnInit(): void {
+    this.addHeaderContent();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  addHeaderContent() {
+    this.breadCrumbs = [
+      {
+        url: '',
+        label: 'Catalogues',
+      },
+      {
+        url: '',
+        label: 'Articles'
+      }
+    ]
+  }
 }
