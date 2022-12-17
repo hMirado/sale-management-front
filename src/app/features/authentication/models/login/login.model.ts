d424644dddd12760c03985f56c3f401178fb4082
Hobiniaina Mirado RAMAHATSANGIARISON <miradotsangy@gmail.com>
2022-12-17 12:22:23 +0300

    update login parametter

--- src/app/features/authentication/models/login/login.model.ts
+++ src/app/features/authentication/models/login/login.model.ts
@@ -1,4 +1,4 @@
 export class Login {
-  email: string;
+  emailOrPhoneNumber: string;
   password: string;
 }
--- src/app/features/authentication/pages/login/login.component.ts
+++ src/app/features/authentication/pages/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { responseStatus } from 'src/app/core/config/constant';
 import { ApiResponse } from 'src/app/core/models/api-response/api-response.model';
-import { NotificationService } from 'src/app/core/services/notification/notification.service';
 import { tokenKey } from 'src/app/shared/config/constant';
 import { HelperService } from 'src/app/shared/serives/helper/helper.service';
 import { LocalStorageService } from 'src/app/shared/serives/local-storage/local-storage.service';
@@ -39,7 +38,7 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   createForm() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      emailOrPhoneNumber: ['', Validators.required],
       password: ['', Validators.required],
     })
   }
@@ -47,11 +46,11 @@ export class LoginComponent implements OnInit, OnDestroy {
   login() {
     this.hasError = !this.loginForm.valid;
     if (!this.hasError) {
-      const user  = new Login();
-      user.email = this.loginForm.value.email;
-      user.password = this.loginForm.value.password;
+      const loginValue  = new Login();
+      loginValue.emailOrPhoneNumber = this.loginForm.value.emailOrPhoneNumber;
+      loginValue.password = this.loginForm.value.password;
 
-      this.authService.login(user).subscribe((response: ApiResponse) => {
+      this.authService.login(loginValue).subscribe((response: ApiResponse) => {
         if (response.status === responseStatus.success) {
           this.getUserData(response.data);
         }
