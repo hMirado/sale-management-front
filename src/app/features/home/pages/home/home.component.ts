731321ac26e3658880541687319e75219f2deaf0
Mirado Tsangy <miradotsangy@gmail.com>
2023-06-01 21:01:59 +0300

    add button make sale and open shop

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadCrumb } from 'src/app/shared/models/bread-crumb/bread-crumb.model';
+import { Button } from 'src/app/shared/models/button/button.model';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,13 @@ import { BreadCrumb } from 'src/app/shared/models/bread-crumb/bread-crumb.model'
 export class HomeComponent implements OnInit {
   public title: string = 'Tableau de bord';
   public breadCrumbs: BreadCrumb[] = [];
-
+  public buttonMakeSale: Button;
+  public buttonOpenShop: Button;
   constructor() { }
 
   ngOnInit(): void {
-    this.addHeaderContent
+    this.addHeaderContent();
+    this.configButton();
   }
 
   addHeaderContent() {
@@ -23,5 +26,24 @@ export class HomeComponent implements OnInit {
         label: 'Tableau de bord'
       },
     ]
+  }
+
+  configButton() {
+    this.buttonOpenShop = {
+      id: 'open',
+      label: 'Ouvrir shop',
+      color: 'primary',
+      action: this.openShop
+    };
+    this.buttonMakeSale ={
+      id: 'sale',
+      label: 'Faire une vente',
+      color: 'secondary'
+    }
+  }
+
+  public shopIsOpen: boolean = false;
+  openShop = () => {
+    this.shopIsOpen = true;
   }
 }
